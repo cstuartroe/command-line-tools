adb044b0b1efbe8d2d13da47b31355dc6299de7d
Conor <cstuartroe@haverford.edu>
2019-02-06 00:15:52 +0000

    some improvements

--- archive.py
+++ archive.py
@@ -12,34 +12,41 @@ parser.add_argument('dirpath', metavar="path/to/dir", type=str, nargs="?",
 parser.add_argument('skips', metavar="<skip dir>", type=list, nargs="*",
                     help = "directories to skip over")
 
-def included(dirpath,skips):
+def included(path,skips):
     for skip in skips:
-        if dirpath.endswith(skip):
+        if path.endswith(skip):
             return False
     return True
 
 def dir_json(dirpath,skips):
-    obj = {"files":{},"dirs":{}}
+    obj = {"files":{},"dirs":{},"total_files":0,"total_dirs":0,"total_size":0}
     
     for item in os.listdir(dirpath):
         fullpath = os.path.join(dirpath,item)
-        
-        if os.path.isfile(fullpath):
-            stats = os.stat(fullpath)
-            obj["files"][item] = {"size":stats.st_size,
-                                  "mode":stats.st_mode,
-                                  "created":datetime.fromtimestamp(stats.st_ctime).strftime(DATE_FORMAT),
-                                  "last modified":datetime.fromtimestamp(stats.st_mtime).strftime(DATE_FORMAT),
-                                  "owner":stats.st_uid}
-            
-        elif included(fullpath,skips):
-            obj["dirs"][item] = dir_json(fullpath,skips)
+        if included(fullpath, skips):
+            if os.path.isfile(fullpath):
+                obj["total_files"] += 1
+                stats = os.stat(fullpath)
+                obj["files"][item] = {"size":stats.st_size,
+                                      "mode":stats.st_mode,
+                                      "created":datetime.fromtimestamp(stats.st_ctime).strftime(DATE_FORMAT),
+                                      "last modified":datetime.fromtimestamp(stats.st_mtime).strftime(DATE_FORMAT),
+                                      "owner":stats.st_uid}
+                obj["total_size"] += stats.st_size
+                
+            else:
+                obj["total_dirs"] += 1
+                subdir_json = dir_json(fullpath,skips)
+                obj["dirs"][item] = subdir_json
+                obj["total_files"] += subdir_json["total_files"]
+                obj["total_dirs"] += subdir_json["total_dirs"]
+                obj["total_size"] += subdir_json["total_size"]
             
     return obj
 
 def archive_dir(args):
     archive_filename = os.path.join(args.dirpath,"archive.json")
-    skips = args.skips + [".git","venv","__pycache__"]
+    skips = args.skips + [".git","venv","__pycache__",".drivedownload"]
     
     try:
         with open(archive_filename,"r",encoding="utf-8") as fh:
